253bb178578b1157c368ebb09322c122e7ec83a7
ramkrishna88 <chowdaryrk077@gmail.com>
2024-01-27 22:59:48 +0000

    initial

---
+++ App.js
@@ -0,0 +1,12 @@
+import {Text, SafeAreaView} from 'react-native';
+import React from 'react';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Text>Hello</Text>
+    </SafeAreaView>
+  );
+};
+
+export default App;
