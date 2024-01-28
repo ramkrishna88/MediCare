f1d6f80ba64437035502fcd626c324ddca465b49
ramkrishna88 <chowdaryrk077@gmail.com>
2024-01-28 03:07:35 +0000

    profile

--- src/screens/home/index.js
+++ src/screens/home/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import {View, ImageBackground, StyleSheet} from 'react-native';
 import {
-  faCoffee,
   faCalendarCheck,
   faUser,
   faArrowRight,
--- src/screens/profile/index.js
+++ src/screens/profile/index.js
@@ -1,12 +1,91 @@
-import {View, Text} from 'react-native';
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
+import firestore from '@react-native-firebase/firestore';
+import auth from '@react-native-firebase/auth';
 
 const ProfileScreen = () => {
+  const [userData, setUserData] = useState({});
+
+  const user = auth().currentUser;
+
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        const userDocument = await firestore()
+          .collection('users')
+          .doc(user.uid)
+          .get();
+        console.log('User Document Data:', userDocument.data());
+
+        if (userDocument.exists) {
+          setUserData(userDocument.data());
+        } else {
+          console.warn('User document does not exist');
+        }
+      } catch (error) {
+        console.error('Error fetching user data:', error.message);
+      }
+    };
+
+    fetchUserData();
+  }, [user.uid]); // Now, user.uid is included in the dependency array
+
   return (
-    <View>
-      <Text>ProfileScreen</Text>
+    <View style={styles.container}>
+      {/* User Info Card */}
+      <View style={styles.card}>
+        <Text style={styles.title}>{userData.name}</Text>
+        <Text style={styles.subtitle}>{userData.email}</Text>
+        <Text style={styles.info}>Contact: {userData.contact}</Text>
+        <Text style={styles.info}> Location: {userData.location}</Text>
+        <Text style={styles.info}> Specialty: {userData.specialty}</Text>
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'flex-start',
+    alignItems: 'flex-start',
+  },
+  card: {
+    width: '96%',
+    padding: 16,
+    borderRadius: 10,
+    backgroundColor: '#fff',
+    elevation: 3,
+    shadowColor: '#000',
+    shadowOffset: {width: 0, height: 2},
+    shadowOpacity: 0.2,
+    shadowRadius: 3,
+    margin: 10,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  subtitle: {
+    fontSize: 18,
+    color: '#666',
+    marginBottom: 1,
+  },
+  info: {
+    fontSize: 16,
+    color: '#333',
+    marginBottom: 10,
+  },
+  button: {
+    backgroundColor: '#007BFF',
+    padding: 10,
+    borderRadius: 5,
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: '#fff',
+    fontWeight: 'bold',
+  },
+});
 
 export default ProfileScreen;
